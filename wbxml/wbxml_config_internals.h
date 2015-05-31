13311d1a052d37eded6028c2bae946c323a1d4dd
Andreas Sandberg <andreas.a.sandberg@gmail.com>
2015-05-30 20:53:40 -0700

    init commit

---
+++ wbxml/wbxml_config_internals.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2008 Michael Bell <michael.bell@opensync.org>
+ */
+
+#ifndef WBXML_CONFIG_INTERNALS_H
+#define WBXML_CONFIG_INTERNALS_H
+
+#define HAVE_EXPAT
+#include <expat.h>
+
+#endif /* WBXML_CONFIG_INTERNALS_H */
